3430b49f03fea871e75bc65ae911f60b15b8bc05
risi1006 <risi1006@gmail.com>
2020-11-04 23:09:21 +0530

    Max freq word in string array

---
+++ GFG DSA Solution/Tries/6th_Max_freq_word.cpp
@@ -0,0 +1,84 @@
+/*
+999. Most frequent word in an array of strings
+
+Given an array arr containing N words consisting of lowercase characters. Your task is to find the most frequent word in the array. If multiple words have same frequency, then print the word whose first occurence occurs last in the array as compared to the other strings with same frequency.
+*/
+
+
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string mostFrequentWord(string arr[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        string arr[n];
+        for (int i = 0; i < n; i++) cin >> arr[i];
+
+        cout << mostFrequentWord(arr, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+// User function template for C++
+
+// arr : given array of string
+// n : number of string in the array
+// return the expected answer
+struct TrieNode{
+  int encounteredAt = 0;
+  map<char,TrieNode *> mp;
+  int count = 0;
+  bool isEnd = false;
+};
+
+int position=-1, cnt = 0;
+string curr;
+
+void insert(TrieNode *root, string key, int pos){
+    for(int i=0; i<key.size(); i++){
+        char c = key[i];
+        if(!root->mp[c])
+            root->mp[c] = new TrieNode();
+            
+        if(root->mp[c])
+            root = root->mp[c];
+    }
+    if(!root->isEnd)
+    {
+        root->encounteredAt = pos;
+        root->isEnd = true;
+    }
+    root->count++;
+    if(root->count > cnt){
+        position = root->encounteredAt;
+        curr = key;
+        cnt = root->count;
+    }
+    else if(root->count == cnt){
+        position = root->encounteredAt;
+        curr = key;
+    }
+}
+
+string mostFrequentWord(string arr[], int n) {
+    TrieNode *root = new TrieNode();
+    for(int i=0;i<n;i++)
+        insert(root, arr[i], i+1);
+    return curr;
+}
+
+
+
+
+
